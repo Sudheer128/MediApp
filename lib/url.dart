6827162b52c07965ed79250eec614b21cfcb56f2
SyedShashavali <syedshashavali144@gmail.com>
2025-05-31 12:24:23 +0530

    seventh

--- lib/url.dart
+++ lib/url.dart
@@ -1,3 +1,3 @@
-const baseurl = "https://med-connect1-644602495453.asia-south1.run.app";
+const baseurl = "http://192.168.0.103:8080";
 // "https://med-connect1-644602495453.asia-south1.run.app";
 //http://192.168.0.103:8080
